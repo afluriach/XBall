8954d04ee8c3d3cd5682a345ab20787268d16e1b
Toni <afluriach@gmail.com>
2014-11-14 09:45:10 -0500

    first commit, LibGDX 1.4.1 hello world desktop and Android

---
+++ desktop/src/com/electricsunstudio/xball/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.electricsunstudio.xball.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.electricsunstudio.xball.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
